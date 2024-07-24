111faa0c9595dfd783691ab426cfb0c7df4a89cf
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-07-24 10:00:01 +0200

    added form in NewEvent component

--- front-end/src/components/Body/NewEvent.vue
+++ front-end/src/components/Body/NewEvent.vue
@@ -3,11 +3,43 @@
 </script>
 
 <template>
-  <h1 class="text-purple-600">new event</h1>
+  <div class="container min-h-full mx-auto p-5">
+  
+    <form class="flex flex-col content-start mx-auto rounded-lg" action="">
+      <h1 class="rounded-t-lg p-1">Evento</h1>
+      <div class="p-3">
+      
+        <div class="flex flex-col">
+          <label for="name">Inserisci il nome dell'evento</label>
+          <input type="text" name="name" id="">
+          <label for="description">Inserisci la descrizione dell'evento</label>
+          <textarea name="description" id="" cols="10" rows="5"></textarea>
+          <label for=""></label>
+          <input type="file" name="" id="">
+          <label for=""></label>
+          <input type="color" name="" id="">
+        </div>
+        <div class="flex">
+          <input type="date" name="" id="">
+          <input type="date" name="" id="">      
+        </div>
+      </div>
+    </form>
+  </div>
 
   
 </template>
 
 <style scoped>
-
+  input, textarea, form{
+    margin-bottom: 5px;
+    border: 1px solid gray;
+  }
+  h1{
+    border-bottom: 1px solid gray;
+    background-color: rgb(236, 235, 235);
+  }
+  form{
+    width: 60%;
+  }
 </style>
