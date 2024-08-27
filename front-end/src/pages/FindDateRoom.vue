f066e56c7d80e77d96c382e6163fe4ee8d0d29ea
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-08-27 12:18:20 +0200

    add style for the title

--- front-end/src/pages/FindDateRoom.vue
+++ front-end/src/pages/FindDateRoom.vue
@@ -3,9 +3,7 @@
 </script>
 
 <template>
-  <h1 class="text-purple-600">finddate</h1>
-
-  
+  <h1 class="text-2xl font-semibold text-center pt-4">Seleziona una data ed una sala per visualizzare gli eventi disponibili</h1>
 </template>
 
 <style scoped>
