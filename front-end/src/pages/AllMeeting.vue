eacb4df26e142fdd06de7341261cd0cc70bc994b
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-08-29 10:08:26 +0200

    added loader

--- front-end/src/pages/AllMeeting.vue
+++ front-end/src/pages/AllMeeting.vue
@@ -5,11 +5,11 @@ import{ref,onMounted} from 'vue';
 
 
 let meetings = ref([])
-
+const loading=ref(true);
 function meetingList(){
   axios.get("http://127.0.0.1:8000/api/meetings").then(result=>{
     meetings.value = result.data.results
-    console.log(meetings.value)
+    loading.value=false;
 })
 }
 
@@ -23,15 +23,22 @@ onMounted(() => {
     
     <h1 class="text-2xl font-semibold text-center pt-4">Lista di tutte le sale meeting registrate</h1>
     
-    <div class="flex flex-row mt-6 gap-2" v-for="element in meetings" :key="element.id">
+    <div v-if="loading == false" class="flex flex-row mt-6 gap-2" v-for="element in meetings" :key="element.id">
       <singleMeeting :meeting="element" />
+    </div>
+    <div v-else class="container mx-auto mt-5 flex justify-center">
+
+        <font-awesome-icon :icon="['fas', 'spinner']"class="fa-spin loader" />
+
     </div>
 </div>
   
 </template>
 
 <style scoped>
-.weq{
-  width: calc(100% / 3);
+
+.loader{
+  min-width: 500px;
+  height: 200px;
 }
 </style>
--- front-end/src/pages/WeekView.vue
+++ front-end/src/pages/WeekView.vue
@@ -12,12 +12,13 @@ today=today.toLocaleDateString();
 
 //axios call to take all week events
 
-
+const loading=ref(true);
 let events = ref([])
 
 function weekElements(){
   axios.get("http://127.0.0.1:8000/api/events/week").then(result=>{
   events.value = result.data.results
+  loading.value=false;
 })
 }
 
@@ -30,14 +31,18 @@ onMounted(() => {
   <div class="container mx-auto">
     
     <h1 class="text-2xl font-semibold text-center pt-4">Ecco una lista degli eventi che vanno da giorno {{ today }} fino al giorno {{ todayPlusSeven }}</h1>
-    
+    <div v-if="loading" class="container mx-auto mt-5 flex justify-center">
+
+      <font-awesome-icon :icon="['fas', 'spinner']"class="fa-spin loader" />
+
+    </div>
     <div v-if="events" class="flex flex-row mt-6 gap-2">
       <div class="border border-black weq p-3 text-center" v-for="element in events" :key="element.id">
         <singleEvent :events="element"/>
       </div>
     </div>
     <div v-else>
-      <h1>aaaaaaa</h1>
+      <h1>Nei prossimi 7 giorni non risultano eventi in corso</h1>
     </div>
 </div>
   
@@ -46,5 +51,10 @@ onMounted(() => {
 <style scoped>
 .weq{
   width: calc(100% / 3);
+}
+
+.loader{
+  min-width: 500px;
+  height: 200px;
 }
 </style>
